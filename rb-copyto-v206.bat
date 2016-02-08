2943807de75826a9a157ac46db82bd4e54ff7fec
dgleba <dgleba@gmail.com>
2016-02-08 14:05:03 -0500

    m

--- rb-copyto-v206.bat
+++ rb-copyto-v206.bat
@@ -1,5 +1,11 @@
 mkdir c:\temp\rblogs
 
-robocopy C:\n\Dropbox\csd\VCS-git\cif207 \\v206b2\html\python\cif207   /e  /COPY:DT /xd .git templates_c testnobackup /fft /dst /xo /ndl /np /r:0 /w:9 /tee /log:"c:\temp\rblogs\cif207-%dhms%-%random%-%random%"
+xcopy C:\n\Dropbox\csd\VCS-git\pdb218 \\PMDS-DATA\p2\xampp\htdocs\python\pdb218   /s/e/d/h/i/f/c/k/y 
+
+robocopy C:\n\Dropbox\csd\VCS-git\pdb218 \\PMDS-DATA\p2\xampp\htdocs\python\pdb218  /l  /COPY:DT /xd  templates_c /fft /dst /xo /ndl /np /r:0 /w:9 /tee /log:"c:\temp\rblogs\pdb218-%dhms%-%random%-%random%"
+
+:robocopy C:\n\Dropbox\csd\VCS-git\pdb218 \\PMDS-DATA\p2\xampp\htdocs\python\pdb218   /e  /COPY:DT /xd .git templates_c testnobackup /fft /dst /xo /ndl /np /r:0 /w:9 /tee /log:"c:\temp\rblogs\pdb218-%dhms%-%random%-%random%"
+
+robocopy C:\n\Dropbox\csd\VCS-git\pdb218 \\v206b2\html\python\pdb218   /e  /COPY:DT /xd .git templates_c testnobackup /fft /dst /xo /ndl /np /r:0 /w:9 /tee /log:"c:\temp\rblogs\pdb218-%dhms%-%random%-%random%"
 
 pause
