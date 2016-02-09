f5697e69ecd32e479841ac1ec9a34cc489fd19c0
dgleba <dgleba@gmail.com>
2016-02-09 08:54:09 -0500

    change template

--- rb-copyto-v206.bat
+++ rb-copyto-v206.bat
@@ -1,6 +1,7 @@
 mkdir c:\temp\rblogs
 
 xcopy C:\n\Dropbox\csd\VCS-git\pdb218 \\PMDS-DATA\p2\xampp\htdocs\python\pdb218   /s/e/d/h/i/f/c/k/y 
+xcopy C:\n\Dropbox\csd\VCS-git\pdb218 \\pmdsdata3\html\python\pdb218              /s/e/d/h/i/f/c/k/y 
 
 robocopy C:\n\Dropbox\csd\VCS-git\pdb218 \\PMDS-DATA\p2\xampp\htdocs\python\pdb218  /l  /COPY:DT /xd  templates_c /fft /dst /xo /ndl /np /r:0 /w:9 /tee /log:"c:\temp\rblogs\pdb218-%dhms%-%random%-%random%"
 
